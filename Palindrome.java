afe383a883c2c98ce15a1a7072d3317c71a19b83
Sai Koushik <koushiksai574@gmail.com>
2025-07-20 19:09:27 +0530

    add support for sentence palindrome check

--- Palindrome.java
+++ Palindrome.java
@@ -1,10 +1,23 @@
+//public class Palindrome {
+//    public static boolean isPalindrome(String str) {
+//        String reversed = new StringBuilder(str).reverse().toString();
+//        return str.equalsIgnoreCase(reversed);
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(isPalindrome("malayalam"));  // true
+//    }
+//}
+
 public class Palindrome {
     public static boolean isPalindrome(String str) {
-        String reversed = new StringBuilder(str).reverse().toString();
-        return str.equalsIgnoreCase(reversed);
+        String cleanStr = str.replaceAll("\\s+", ""); // remove all whitespaces
+        String reversed = new StringBuilder(cleanStr).reverse().toString();
+        return cleanStr.equalsIgnoreCase(reversed);
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("malayalam"));  // true
+        System.out.println(isPalindrome("malayalam"));           // true
+        System.out.println(isPalindrome("nurses run"));      // true
     }
 }
